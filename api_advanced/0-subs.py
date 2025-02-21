8702295bc303032490812b4d9bb6ab446071b64d
A-Hirwa <a.hirwa1@alustudent.com>
2025-02-21 15:52:30 +0200

    new

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 """DOC"""
-=======
-"""Return number of subscribers for a given subreddit"""
->>>>>>> 392a0234b0d8a1a499f86f9d724ff448dab57edb
 import requests
 
 
 def number_of_subscribers(subreddit):
-<<<<<<< HEAD
     """DOC"""
     reddit_url = "https://www.reddit.com/r/{}/about.json" \
         .format(subreddit)
@@ -22,14 +17,4 @@ def number_of_subscribers(subreddit):
         data = response.json()['data']
         subs = data['subscribers']
         return subs
-=======
-    """Return the number of subscribers """
-    url = "https://www.reddit.com/r/{}/about.json" \
-        .format(subreddit)
-    headers = {'User-Agent': 'My User Agent 1.0'}
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        return response.json().get('data') \
-            .get('subscribers')
->>>>>>> 392a0234b0d8a1a499f86f9d724ff448dab57edb
     return 0
